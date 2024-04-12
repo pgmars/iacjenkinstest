2c30f2163af18592e29d25f9e32e2d55e6faa9fa
CuakJr <140828381+CuakJr@users.noreply.github.com>
2024-04-12 18:15:06 +0530

    Add files via upload

---
+++ var.tf
@@ -0,0 +1,13 @@
+variable "AWS_ACCESS_KEY" {
+  type    = string
+  default = "AKIAZI2LDRBZUXDUUC5M"
+}
+
+variable "AWS_SECRET_KEY" {
+  type = string
+  default = "4gUZywyfkFvEcJTtjhS6R8QwFiTNzyonFlE6us/t"
+}
+
+variable "AWS_REGION" {
+  default = "ap-south-1"
+}
